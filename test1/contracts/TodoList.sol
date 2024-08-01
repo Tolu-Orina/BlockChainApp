eddf62b83003ca497f57db763733bec74195c066
Tolulope Orina <toluorina@gmail.com>
2024-08-01 22:02:12 +0100

    Adding test1 blockchain app

---
+++ test1/contracts/TodoList.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity 0.5.16;
+
+contract TodoList {
+    // State variables
+    // They are written to the blockchain
+    uint public taskCount = 0;
+
+}
