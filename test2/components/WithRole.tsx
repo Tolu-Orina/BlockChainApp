c6f38b721a33185d932c8aa5208470ae19713854
Tolulope Orina <toluorina@gmail.com>
2024-09-01 19:56:44 +0100

    Commented out WithRole

--- test2/components/WithRole.tsx
+++ test2/components/WithRole.tsx
@@ -1,35 +1,35 @@
-// hoc/withRole.tsx
-'use client';
+// // hoc/withRole.tsx
+// 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { currentAuthenticatedUser } from './authUtils';
-import { useRouter } from 'next/navigation';
+// import React, { useEffect, useState } from 'react';
+// import { currentAuthenticatedUser } from './authUtils';
+// import { useRouter } from 'next/navigation';
 
-const WithRole = (role: string, Component: React.ComponentType) => {
-  return (props: any) => {
-    const [authorized, setAuthorized] = useState(false);
-    const router = useRouter();
+// const WithRole = (role: string, Component: React.ComponentType) => {
+//   return (props: any) => {
+//     const [authorized, setAuthorized] = useState(false);
+//     const router = useRouter();
 
-    useEffect(() => {
-      currentAuthenticatedUser()
-        .then(currentUserOutput => {
-          const { username, userId, signInDetails } = currentUserOutput;
-          console.log(username, userId, signInDetails)
+//     useEffect(() => {
+//       currentAuthenticatedUser()
+//         .then(currentUserOutput => {
+//           const { username, userId, signInDetails } = currentUserOutput;
+//           console.log(username, userId, signInDetails)
 
-          // const userGroups = user.signInUserSession.accessToken.payload['cognito:groups'] || [];
-          // if (userGroups.includes(role)) {
-          //   setAuthorized(true);
-          // } else {
-          //   router.push('/unauthorized');
-          // }
-        })
-        .catch(() => router.push('/login'));
-    }, [router, role]);
+//           // const userGroups = user.signInUserSession.accessToken.payload['cognito:groups'] || [];
+//           // if (userGroups.includes(role)) {
+//           //   setAuthorized(true);
+//           // } else {
+//           //   router.push('/unauthorized');
+//           // }
+//         })
+//         .catch(() => router.push('/login'));
+//     }, [router, role]);
 
-    if (!authorized) return null;
+//     if (!authorized) return null;
 
-    return <Component {...props} />;
-  };
-};
+//     return <Component {...props} />;
+//   };
+// };
 
-export default WithRole;
+// export default WithRole;
